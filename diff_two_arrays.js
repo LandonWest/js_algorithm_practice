4b98b50de7a3224f80083a621029b0c5ad29608a
Landon West <landonwest5@gmail.com>
2017-06-08 21:50:37 -0700

    create function that returns differences between two arrays

---
+++ diff_two_arrays.js
@@ -0,0 +1,35 @@
+// Compare two arrays and return a new array with any items only found in one of
+// the two given arrays, but not both. In other words, return the symmetric
+// difference of the two arrays.
+
+// First Attempt:
+function diffArray(arr1, arr2) {
+  var newArr = [];
+
+  arr1.forEach(indexA => {
+    if (arr2.includes(indexA) == false) {
+      newArr.push(indexA);
+    }
+  });
+
+  arr2.forEach(indexB => {
+    if (arr1.includes(indexB) == false) {
+      newArr.push(indexB);
+    }
+  });
+
+  return newArr;
+}
+
+// diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+
+// Tests:
+// diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) should return an array.
+// ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["pink wool"].
+// ["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["diorite", "pink wool"].
+// ["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return [].
+// [1, 2, 3, 5], [1, 2, 3, 4, 5] should return [4].
+// [1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return ["piglet", 4].
+// [], ["snuffleupagus", "cookie monster", "elmo"] should return ["snuffleupagus", "cookie monster", "elmo"].
+// [1, "calf", 3, "piglet"], [7, "filly"] should return [1, "calf", 3, "piglet", 7, "filly"].
