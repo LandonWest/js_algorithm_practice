524889c9f4fe8446fdfd40e8b5e4ab76d428e990
Landon West <landonwest5@gmail.com>
2017-07-15 19:33:01 -0700

    refactor diffTwoArrays & create algorithm for symmetrical diff of 2 or more arrays

--- diff_two_arrays.js
+++ diff_two_arrays.js
@@ -43,11 +43,9 @@ function diffArray(arr1, arr2) {
 
 // SOLUTION from fcc forum
 function diffArray(arr1, arr2) {
-  return arr1
-    .concat(arr2)
-    .filter(
-        item => !arr1.includes(item) || !arr2.includes(item)
-    )
+  return arr1.concat(arr2).filter(item => {
+    return !arr1.includes(item) || !arr2.includes(item);
+  });
 }
 
 
